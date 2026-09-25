ae96736f660548fc2cd2fed4ce52b9a4ca8dced5
Gayane Hakobyan <gayanehakobyan93@gmail.com>
2026-09-25 15:20:47 +0400

    Implementation posthog

--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -1,9 +1,20 @@
 'use client';
 import Image from 'next/image';
+import posthog from 'posthog-js';
 
 const ExploreBtn = () => {
+  const handleExploreClick = () => {
+    if (
+      process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN &&
+      process.env.NEXT_PUBLIC_POSTHOG_HOST
+    ) {
+      posthog.capture('events_explore_clicked');
+    }
+    console.log('clicked');
+  };
+
   return (
-    <button id='explore-btn' onClick={() => console.log('clicked')}>
+    <button id='explore-btn' onClick={handleExploreClick}>
       <a href='#events'>Explore events</a>
       <Image src='/icons/arrow-down.svg' alt='arrow-down' width={24} height={24} />
     </button>
